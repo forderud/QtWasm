7494da5b6b6b4d3cbe67ea9c987ce0fae7de3eaf
Fredrik Orderud <fredrik.orderud@ge.com>
2019-05-10 11:07:12 +0200

    Import Qt/QML plugins for CMake compatibility
    These plugins are already auto-imported when using QMake.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtPlugin>
+
+#if QT_FEATURE_static > 0
+// import Qt plugins (needed for CMake builds)
+Q_IMPORT_PLUGIN(QWasmIntegrationPlugin)
+Q_IMPORT_PLUGIN(QtQuick2Plugin)       // QtQuick
+Q_IMPORT_PLUGIN(QtQuick2WindowPlugin) // QtQuick.Window
+#endif
 
 int main(int argc, char *argv[])
 {
