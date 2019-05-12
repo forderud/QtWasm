585e4dbbdb5c3170ac492b4e8292e5192331863b
Fredrik Orderud <fredrik.orderud@ge.com>
2019-05-12 11:25:37 +0200

    .

--- source/wasm.cmake
+++ source/wasm.cmake
@@ -1,6 +1,4 @@
 # Add WebAssembly compile & linker flags:
-#add_link_options(--bind)
-#target_link_options(helloworld --bind)
 # USE_WEBGL2:          https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#webgl-friendly-subset-of-opengl-es-2-0-3-0
 # FULL_ES2/3:          Emulate missing WebGL features (https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#opengl-es-2-0-3-0-emulation)
 # ALLOW_MEMORY_GROWTH: https://emscripten.org/docs/optimizing/Optimizing-Code.html#memory-growth
@@ -9,3 +7,6 @@
 set(WASM_FLAGS "-s USE_WEBGL2=1 -s FULL_ES2=1 -s FULL_ES3=1 -s EXIT_RUNTIME=1 -s ALLOW_MEMORY_GROWTH=1 -s \"BINARYEN_TRAP_MODE='clamp'\"")
 
 add_definitions(${WASM_FLAGS})
+
+#add_link_options(--bind ${WASM_FLAGS})
+#target_link_options(helloworld --bind ${WASM_FLAGS})
