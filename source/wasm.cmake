8c7ffd328f189a43db4ed45cb9d52f94e32becb6
Fredrik Orderud <fredrik.orderud@ge.com>
2019-05-13 13:35:58 +0200

    Remove redundant compiler defines that did not affect the generated binaries.

--- source/wasm.cmake
+++ source/wasm.cmake
@@ -1,21 +1,27 @@
-# Add WebAssembly compile & linker flags:
-# USE_WEBGL2:          https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#webgl-friendly-subset-of-opengl-es-2-0-3-0
-# FULL_ES2/3:          Emulate missing WebGL features (https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#opengl-es-2-0-3-0-emulation)
-# EXIT_RUNTIME:        Run static dtors (https://emscripten.org/docs/getting_started/FAQ.html#what-does-exiting-the-runtime-mean-why-don-t-atexit-s-run)
-# ALLOW_MEMORY_GROWTH: https://emscripten.org/docs/optimizing/Optimizing-Code.html#memory-growth
-# BINARYEN_TRAP_MODE:  Prevent overflow from trapping (https://emscripten.org/docs/compiling/WebAssembly.html#trap-mode)
+# Configure WebAssembly build settings
+# For some reason, the build settings need to be provided through the linker.
 
-add_definitions("-s USE_WEBGL2=1")
-add_definitions("-s FULL_ES2=1")
-add_definitions("-s FULL_ES3=1")
-add_definitions("-s EXIT_RUNTIME=1")
-add_definitions("-s ALLOW_MEMORY_GROWTH=1")
-add_definitions("-s \"BINARYEN_TRAP_MODE='clamp'\"")
+# Activate Embind C/C++ bindings
+# https://emscripten.org/docs/porting/connecting_cpp_and_javascript/embind.html
+add_link_options(--bind) 
 
-add_link_options(--bind)
+# Activate WebGL 2 (in addition to WebGL 1)
+# https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#webgl-friendly-subset-of-opengl-es-2-0-3-0
 add_link_options("SHELL:-s USE_WEBGL2=1")
+
+# Emulate missing OpenGL ES2/ES3 features
+# https://emscripten.org/docs/porting/multimedia_and_graphics/OpenGL-support.html#opengl-es-2-0-3-0-emulation
 add_link_options("SHELL:-s FULL_ES2=1")
 add_link_options("SHELL:-s FULL_ES3=1")
+
+# Run static dtors at teardown
+# https://emscripten.org/docs/getting_started/FAQ.html#what-does-exiting-the-runtime-mean-why-don-t-atexit-s-run
 add_link_options("SHELL:-s EXIT_RUNTIME=1")
+
+# Allows amount of memory used to change 
+# https://emscripten.org/docs/optimizing/Optimizing-Code.html#memory-growth
 add_link_options("SHELL:-s ALLOW_MEMORY_GROWTH=1")
+
+# Prevent overflow from trapping
+# https://emscripten.org/docs/compiling/WebAssembly.html#trap-mode
 add_link_options("SHELL:-s \"BINARYEN_TRAP_MODE='clamp'\"")
