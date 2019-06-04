21e27fd2b21374df8ac21ce3841f3ccb299d65f7
Fredrik Orderud <forderud@gmail.com>
2019-06-04 19:48:23 +0200

    Get rid of separate qmake image.

--- image/build_qmake.bat
+++
@@ -1,8 +0,0 @@
-:: Build container
-docker build --file Dockerfile.qmake --tag=forderud/wasmbuilder:qmake .
-
-:: Push to dockerhub
-docker tag forderud/wasmbuilder:qmake forderud/wasmbuilder:qmake
-docker push forderud/wasmbuilder:qmake
-
-pause
--- run_qmake.bat
+++ run_qmake.bat
@@ -1,5 +1,5 @@
 mkdir C:\Dev\QtWasm\build
 
-docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/wasmbuilder:qmake
+docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/wasmbuilder:cmake /bin/bash -c "qmake /project/source && make"
 
 pause
