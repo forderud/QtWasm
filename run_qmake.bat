27e7d62e4a92c11f6b95eb3cb7a2c62ff07eb301
Fredrik Orderud <forderud@gmail.com>
2019-09-23 14:26:45 +0200

    Switch to building Qt 5.14
    Doesn't work yet due to https://bugreports.qt.io/browse/QTBUG-79547

--- run_qmake.bat
+++ run_qmake.bat
@@ -2,6 +2,6 @@ mkdir C:\Dev\QtWasm\build
 
 :: Input sources must be found in C:\Dev\QtWasm\source
 :: Output binaries will be written to C:\Dev\QtWasm\build
-docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/usr/local/Qt-5.13.2/bin;qmake /project/source && make"
+docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/usr/local/Qt-5.14.1/bin;qmake /project/source && make"
 
 pause
