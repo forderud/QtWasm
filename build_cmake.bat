7a897fea3ae54e7119cfd08461b296215c402b01
Fredrik Orderud <forderud@gmail.com>
2019-10-24 21:18:02 +0200

    Use a separate "debug" tag for debug builds of Qt.

--- build_cmake.bat
+++ build_cmake.bat
@@ -5,6 +5,6 @@ mkdir build
 
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
-docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest || exit /b 1
+docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:debug || exit /b 1
 
 run_webserver.bat
