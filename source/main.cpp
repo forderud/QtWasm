7df3118b870c688fde397f64a75dce93ce793a83
Fredrik Orderud <forderud@gmail.com>
2020-01-12 22:18:09 +0100

    Preload file into WASM binary

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtPlugin>
+#include <string>
+#include <fstream>
+#include <iostream>
 
 #if QT_FEATURE_static > 0
 #ifdef EMSCRIPTEN
@@ -11,8 +14,21 @@ Q_IMPORT_PLUGIN(QtQuick2Plugin)       // QtQuick
 Q_IMPORT_PLUGIN(QtQuick2WindowPlugin) // QtQuick.Window
 #endif
 
+
+static void PrintFileContent (const char * filename) {
+    std::ifstream myfile (filename);
+    if (!myfile.is_open())
+        return;
+    
+    std::string line;
+    while (getline(myfile,line))
+        std::cout << line << '\n';
+}
+
 int main(int argc, char *argv[])
 {
+    PrintFileContent("example.txt");
+
     QGuiApplication app(argc, argv);
 
     QCoreApplication::addLibraryPath("./");
