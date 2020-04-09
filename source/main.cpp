a34f833ac020f3435a3833890bab556b657d032f
Fredrik Orderud <forderud@gmail.com>
2020-04-09 13:36:10 +0200

    Remove QCoreApplication::addLibraryPath("./") call that  no longer appear to be needed.

--- source/main.cpp
+++ source/main.cpp
@@ -28,8 +28,6 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    QCoreApplication::addLibraryPath("./");
-
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
