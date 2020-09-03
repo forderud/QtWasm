3bf7b7f0e371871949c17f7de6139e1829a08a71
Fredrik Orderud <fredrik.orderud@ge.com>
2020-09-03 13:58:02 +0200

    WIP: Docker container with VS2019.
    WARNING: Doesn't work yet.

---
+++ msvc/BUILD.bat
@@ -0,0 +1,5 @@
+:: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container?view=vs-2019
+
+docker build -t buildtools2019:latest -m 4GB .
+
+pause
---
+++ msvc/RUN.bat
@@ -0,0 +1,7 @@
+:: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container?view=vs-2019
+
+docker run --rm -it buildtools2019 -m 4GB
+::docker run -it buildtools2019
+
+
+pause
