a0b7d7120f1d6d86bc103bd7f429936b9140e7b8
Fredrik Orderud <forderud@gmail.com>
2020-09-03 22:17:46 +0200

    .

--- msvc/BUILD.bat
+++ msvc/BUILD.bat
@@ -1,4 +1,5 @@
 :: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container?view=vs-2019
+:: https://docs.microsoft.com/en-us/virtualization/windowscontainers/manage-containers/container-storage#storage-limits
 
 docker build -t buildtools2019:latest -m 4GB .
 
