3c759b217309c0c6988da18eb35c7c6aa1b0beaf
Fredrik Orderud <forderud@gmail.com>
2020-09-03 18:53:24 +0200

    fix mem arg.

--- msvc/RUN.bat
+++ msvc/RUN.bat
@@ -1,6 +1,6 @@
 :: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container?view=vs-2019
 
-docker run --rm -it buildtools2019 -m 4GB
+docker run --rm -it -m 4GB buildtools2019
 ::docker run -it buildtools2019
 
 
