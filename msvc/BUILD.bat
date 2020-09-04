3328fa4744218bbf8798c676ccb7161506784bf9
Fredrik Orderud <forderud@gmail.com>
2020-09-04 08:12:55 +0200

    .

--- msvc/BUILD.bat
+++ msvc/BUILD.bat
@@ -1,5 +1,9 @@
 :: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container?view=vs-2019
+
+:: Incraase container disk space:
 :: https://docs.microsoft.com/en-us/virtualization/windowscontainers/manage-containers/container-storage#storage-limits
+:: RUN "fsutil volume diskfree c:"
+
 
 docker build -t buildtools2019:latest -m 4GB .
 
