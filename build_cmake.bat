95015386943b3ac0c40a45cd0770f4cb461afa10
Fredrik Orderud <fredrik.orderud@ge.com>
2022-10-18 18:03:55 +0200

    Get rid of all hardcoded paths.

--- build_cmake.bat
+++ build_cmake.bat
@@ -2,4 +2,4 @@ mkdir build
 
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
-docker run --rm -v C:\Dev\QtWasm\sample:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest
+docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest
--- build_qmake.bat
+++ build_qmake.bat
@@ -2,4 +2,4 @@ mkdir build
 
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
-docker run --rm -v C:\Dev\QtWasm\sample:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source && make"
+docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source && make"
