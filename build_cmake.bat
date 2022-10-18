059eb463ebac0c4f01d5e16d20b8625efb89b71a
Fredrik Orderud <fredrik.orderud@ge.com>
2022-10-18 17:45:15 +0200

    Rename sample project folder from "source" to "sample".

--- build_cmake.bat
+++ build_cmake.bat
@@ -1,5 +1,5 @@
 mkdir C:\Dev\QtWasm\build
 
-:: Input sources must be found in C:\Dev\QtWasm\source
+:: Input sources must be found in C:\Dev\QtWasm\sample
 :: Output binaries will be written to C:\Dev\QtWasm\build
-docker run --rm -v C:\Dev\QtWasm\source:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest
+docker run --rm -v C:\Dev\QtWasm\sample:/project/source -v C:\Dev\QtWasm\build:/project/build forderud/qtwasm:latest
