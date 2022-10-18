a9289ed4a045b32fc8efae41a41c4ebe7266c3cc
Fredrik Orderud <fredrik.orderud@ge.com>
2022-10-18 17:28:54 +0200

    Update web server instructions.

--- python_webserver.bat
+++ python_webserver.bat
@@ -1,2 +1,7 @@
+setlocal enableextensions
+@echo off
 cd build
-python.exe -m http.server 8080
+
+echo Open http://localhost:80 in a web browser and navigate to the html page to test the web app.
+
+python.exe -m http.server 80
