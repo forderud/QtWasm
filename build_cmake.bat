938f0ed24d6822103f279fff3869b92ab8b88e22
Fredrik Orderud <forderud@gmail.com>
2023-01-07 12:11:15 +0100

    Auto-start webserver after build completes.

--- build_cmake.bat
+++ build_cmake.bat
@@ -5,4 +5,6 @@ mkdir build
 
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
-docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest
+docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest || exit /b 1
+
+run_webserver.bat
--- build_qmake.bat
+++ build_qmake.bat
@@ -5,4 +5,6 @@ mkdir build
 
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
-docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source && make"
+docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest /bin/bash -c "export PATH=$PATH:/project/Qt/bin;qmake /project/source && make" || exit /b 1
+
+run_webserver.bat
