23259b4d2f720f5b2b7fab52ad8bd6c7a8293199
Fredrik Orderud <forderud@gmail.com>
2023-02-03 17:13:53 +0100

    Simplify webserver based on suggestion from Morgan Jakobsen.

--- WebServer.py
+++ WebServer.py
@@ -1,16 +1,14 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http import server
 import sys
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
+class CORSRequestHandler (server.SimpleHTTPRequestHandler):
     '''Custom HTTP request handler that adds CORS headers to enable SharedArrayBuffer'''
     def end_headers (self):
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        SimpleHTTPRequestHandler.end_headers(self)
+        server.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
-
-    with HTTPServer(("", port), CORSRequestHandler) as httpd:
-        print("serving at port", port)
-        httpd.serve_forever()
+    print("serving at port", port)
+    server.test(HandlerClass=CORSRequestHandler, port=port)
