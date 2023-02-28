bd83d4cde92670d37123c2151c9a882afb3594e0
Quentin Deldycke <quentin.deldycke@qt.io>
2023-02-28 14:43:26 +0100

    Update scripting to reflect qdel/QtWasm latest modifs

--- build_cmake.sh
+++ build_cmake.sh
@@ -3,4 +3,4 @@
 rm -rf build
 mkdir build
 
-docker run --pull always -ti --rm -v `pwd`/sample:/project/source -v `pwd`/build:/project/build forderud/qtwasm:latest || exit 1
+docker run -ti --rm -v `pwd`/sample:/project/source -v `pwd`/build:/project/build qdel/qtwasm bash -c "/usr/local/Qt-wasm/bin/qt-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja /project/source && ninja -v" || exit 1
--- run_webserver.sh
+++ run_webserver.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd build
-
 echo Opening web browser and starting web server...
-
-(sleep 1 && xdg-open "http://localhost:8080/helloworld.html") &
-python3 ../WebServer.py 8080
+(sleep 1 && xdg-open "http://localhost:8000/helloworld.html") &
+docker run -ti --rm -p 8000:8000 -v `pwd`/sample:/project/source -v `pwd`/build:/project/build -v `pwd`/WebServer.py:/project/WebServer.py qdel/qtwasm bash -c "python3 /project/WebServer.py 8000" || exit 1
