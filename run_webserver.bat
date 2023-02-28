633823f6acf2c25cb13e3ecb03f98624573331e6
Quentin Deldycke <quentin.deldycke@qt.io>
2023-02-28 15:33:00 +0100

    Use container as webserver host

--- run_webserver.bat
+++ run_webserver.bat
@@ -6,4 +6,4 @@ echo Opening web browser and starting web server...
 
 start http://localhost:80/helloworld.html
 
-..\WebServer.py 80
+docker run -ti --rm -p 8000:8000 -v `pwd`/sample:/project/source -v `pwd`/build:/project/build -v `pwd`/WebServer.py:/project/WebServer.py qdel/qtwasm bash -c "python3 /project/WebServer.py 8000"
