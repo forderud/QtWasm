10e19f931aa64ab9b136cd09986a59e60eb83050
Fredrik Orderud <fredrik.orderud@ge.com>
2023-03-17 09:01:14 +0100

    Update Qt WASM URL to new path.

--- sample/wasm.cmake
+++ sample/wasm.cmake
@@ -1,7 +1,7 @@
 # Configure WebAssembly build settings
 # For some reason, the build settings need to be provided through the linker.
 # Most flags are configured to match Qt qmake
-# See EMCC_COMMON_LFLAGS in https://github.com/qt/qtbase/blob/dev/mkspecs/wasm-emscripten/qmake.conf
+# See EMCC_COMMON_LFLAGS in https://github.com/qt/qtbase/blob/dev/mkspecs/common/wasm/wasm.conf
 
 # Also search for packages beneath filesystem root (in addition to /emsdk_portable/sdk/system)
 list(APPEND CMAKE_FIND_ROOT_PATH "/")
