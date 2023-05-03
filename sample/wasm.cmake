97608fb70f3778b88ba64d643b682bebe5eb22ce
Fredrik Orderud <forderud@gmail.com>
2023-05-03 15:03:57 +0200

    Fix issue with Eigen not being detected from /opt/wasm-deps
    Fixes #45.

--- sample/wasm.cmake
+++ sample/wasm.cmake
@@ -4,8 +4,7 @@
 # See EMCC_COMMON_LFLAGS in https://github.com/qt/qtbase/blob/dev/mkspecs/common/wasm/wasm.conf
 
 # Also search for packages beneath filesystem root (in addition to /emsdk_portable/sdk/system)
-list(APPEND CMAKE_FIND_ROOT_PATH "/") # required for Eigen (TODO: Figure out how to get rid of this)
-list(APPEND CMAKE_FIND_ROOT_PATH "/opt/wasm-deps") # required for Boost
+list(APPEND CMAKE_FIND_ROOT_PATH "/opt/wasm-deps") # required for Boost & Eigen
 
 # Increase stack size (was reduced to 64k in Emscripten 3.1.27)
 add_link_options("SHELL:-sSTACK_SIZE=1MB")
