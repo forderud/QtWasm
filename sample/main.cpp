c121fac92bd8c4f99db8795395a81f89a252d1cd
Fredrik Orderud <forderud@gmail.com>
2023-05-04 23:11:50 +0200

    Extend sample project to also leverage HDF5.

--- sample/main.cpp
+++ sample/main.cpp
@@ -5,8 +5,10 @@
 #include <fstream>
 #include <thread>
 #include <iostream>
+
 #include <Eigen/Dense>
 #include <boost/version.hpp>
+#include <hdf5.h>
 
 
 static void PrintFileContent (const char * filename) {
@@ -34,7 +36,8 @@ int main(int argc, char *argv[])
     std::cout << "Qt version " << QT_VERSION_STR << std::endl;
 	std::cout << "Eigen version " << EIGEN_WORLD_VERSION << "." << EIGEN_MAJOR_VERSION  << "." << EIGEN_MINOR_VERSION << std::endl;
 	std::cout << "Boost version " << BOOST_VERSION << std::endl;
-	
+	std::cout << "HDF5 version " << H5_VERSION << std::endl;
+		
 #ifdef NDEBUG
     std::cout << "Release build." << std::endl;
 #else
