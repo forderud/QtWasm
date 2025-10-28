81a57d62161e083ee99a15f391dc5eabd2c461cd
Alf Grini <alf.grini@gehealthcare.com>
2025-10-28 14:29:52 +0100

    Do not use the wasm.cmake from /opt/wasm-deps for the test app
    To avoid problems if running a new version of the sampleapp in older images
    Note that new versions of the sample app will need to run new versions
    of build_cmake.bat or build_cmake.sh

--- build_cmake.sh
+++ build_cmake.sh
@@ -3,4 +3,4 @@
 rm -rf build
 mkdir build
 
-docker run --pull always -ti --rm -v `pwd`/sample:/project/source -v `pwd`/build:/project/build forderud/qtwasm:latest || exit 1
+docker run --pull always -ti --rm -v `pwd`/sample:/project/source -v `pwd`/build:/project/build -v `pwd`/image:/project/image forderud/qtwasm:latest || exit 1
