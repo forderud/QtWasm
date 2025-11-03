45d5ad868bce1b42005898947d4d374110452312
Alf Grini <alf.grini@gehealthcare.com>
2025-11-03 11:36:40 +0100

    Serve the webpage from the docker image in build_cmake.bat
    This is a preparation step to be able to debug the sources.

--- build_cmake.bat
+++ build_cmake.bat
@@ -23,4 +23,16 @@ if "%BUILD_TYPE%"=="" (
   docker run --rm %MOUNT_SOURCE% %MOUNT_BUILD% %DOCKER_IMAGE% /bin/bash -c "/opt/Qt/bin/qt-cmake -DQT_CHAINLOAD_TOOLCHAIN_FILE=/project/source/wasm.cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G Ninja /project/source && ninja" || exit /b 1
 )
 
-run_webserver.bat
+:: Serve the webpage from current folder (%cd% ==> within docker that is /project)
+:: To reflect what was built, we need /project/source and /project/build available
+:: Correct file-mapping will make it possible to attach a debugger later 
+set LOCAL_DEPLOY_FOLDER=%cd%
+set DOCKER_DEPLOY_FOLDER=/project
+
+start http://localhost:8080/build/app/helloworld.html
+
+docker run --rm -p 8080:8080 ^
+    -v %LOCAL_DEPLOY_FOLDER%:%DOCKER_DEPLOY_FOLDER% ^
+    %MOUNT_SOURCE% ^
+    -w %DOCKER_DEPLOY_FOLDER% %DOCKER_IMAGE% ^
+    python3 WebServer.py 8080
