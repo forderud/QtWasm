cae420b343ccc48b1aab0a71dc581dbc3843a3e7
Fredrik Orderud <forderud@gmail.com>
2025-11-04 21:46:33 +0100

    deduplicate docker mount parameters

--- build_cmake.bat
+++ build_cmake.bat
@@ -11,14 +11,15 @@ mkdir build
 :: Valid choices are "Debug", "RelWithDebInfo", "Release" (also default)
 set BUILD_TYPE=%1
 set DOCKER_IMAGE=forderud/qtwasm:latest
+set DOCKER_MOUNT=-v %cd%\sample:/project/source -v %cd%\build:/project/build
 
 if "%BUILD_TYPE%"=="" (
   :: Default build settings
-  docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build %DOCKER_IMAGE% || exit /b 1
+  docker run --rm %DOCKER_MOUNT% %DOCKER_IMAGE% || exit /b 1
 ) else (
   :: Custom build settings
   copy image\wasm.cmake sample
-  docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build %DOCKER_IMAGE% /bin/bash -c "/opt/Qt/bin/qt-cmake -DQT_CHAINLOAD_TOOLCHAIN_FILE=/project/source/wasm.cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G Ninja /project/source && ninja" || exit /b 1
+  docker run --rm %DOCKER_MOUNT% %DOCKER_IMAGE% /bin/bash -c "/opt/Qt/bin/qt-cmake -DQT_CHAINLOAD_TOOLCHAIN_FILE=/project/source/wasm.cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G Ninja /project/source && ninja" || exit /b 1
 )
 
 run_webserver.bat
