1239c117cfc26602919f3d2bdfd37b4e8332d5ed
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-04 18:30:30 +0100

    Extend script to take a BUILD_TYPE argument.

--- build_cmake.bat
+++ build_cmake.bat
@@ -1,3 +1,5 @@
+@echo off
+
 :: clean up any previous builds
 rmdir build /s /q
 
@@ -6,11 +8,16 @@ mkdir build
 :: Input sources must be found in "sample" subfolder
 :: Output binaries will be written to "build" subfolder
 
-:: Default build settings
-docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest || exit /b 1
+:: Valid choices are "Debug", "RelWithDebInfo", "Release" (also default)
+set BUILD_TYPE=%1
 
-:: Custom build settings
-::copy image\wasm.cmake sample
-::docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest /bin/bash -c "/opt/Qt/bin/qt-cmake -DQT_CHAINLOAD_TOOLCHAIN_FILE=/project/source/wasm.cmake -DCMAKE_BUILD_TYPE=Release -G Ninja /project/source && ninja" || exit /b 1
+if "%BUILD_TYPE%"=="" (
+  :: Default build settings
+  docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest || exit /b 1
+) else (
+  :: Custom build settings
+  copy image\wasm.cmake sample
+  docker run --rm -v %cd%\sample:/project/source -v %cd%\build:/project/build forderud/qtwasm:latest /bin/bash -c "/opt/Qt/bin/qt-cmake -DQT_CHAINLOAD_TOOLCHAIN_FILE=/project/source/wasm.cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G Ninja /project/source && ninja" || exit /b 1
+)
 
 run_webserver.bat
